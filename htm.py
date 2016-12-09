1dd92b7b9a54590d9b94ceadd4250f45ff1aa98b
user <xor@slackwerks.com>
2016-12-09 10:20:25 -0500

    periodic update

--- htm.py
+++ htm.py
@@ -12,23 +12,50 @@ def gen_sensor_data():
     sd.append([1,1,0,0,0,1, 1,0])
     return sd
 
-def gen_pooler(dw,w=10,d=10):
+def gen_pooler(data_width,cols=256,cells=64):
     pooler = []
     permanence = []
-    for i in range(0,w):
-        pooler.append([randint(0,1) for x in range(0,d)])
-        permanence.append([random() for x in range(0,dw)])
+    for i in range(0,cols):
+        pooler.append([0 for x in range(0,cells)])
+        permanence.append([random()/4 for x in range(0,data_width)])
     return pooler, permanence
+
+def get_top_cols(scores):
+    score_bucket = {}
+    for i in range(0,len(scores)):
+        if scores[i] not in score_bucket:
+            score_bucket[scores[i]] = []
+        score_bucket[scores[i]].append(i)
+    sorted_scores = sorted(score_bucket.keys())
+    return score_bucket[sorted_scores[-1:][0]]
+    
 
 sensor_data = gen_sensor_data()
 pooler, permanence = gen_pooler(len(sensor_data[0]))
-print(sensor_data)
-print(pooler)
-print(permanence)
 
 # each pooler column sees all sensor_data values
 
 seed(7)
+threshold = 0.2
 for datum in sensor_data:
+    print("Datum: ",datum)
     # compute overlap score for each column
-    pass
+    overlap_scores = []
+    for c in range(0,len(pooler)):
+        score = 0
+        for d in range(0,len(datum)):
+            if 1 == datum[d] and permanence[c][d] > threshold:
+                score += 1
+        overlap_scores.append(score)
+    top_cols = get_top_cols(overlap_scores)
+
+    # update sensor to pooler permanence
+
+    for c in range(0,len(pooler)):
+        for d in range(0,len(datum)):
+            if 1 == datum[d] and permanence[c][d] >= threshold:
+                permanence[c][d] += 0.08
+            elif 0 == datum[2] and permanence[c][d] >= threshold:
+                permanence[c][d] -= 0.03
+
+    # 
